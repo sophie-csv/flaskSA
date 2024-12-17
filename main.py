7b1a3826dab7fe52f592ef9ca420074ec98ce103
sophie-csv <sophieecastellon@gmail.com>
2024-12-17 16:21:11 -0500

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello, world!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
